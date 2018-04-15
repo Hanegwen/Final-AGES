dba37ab1daca2c5b48f86cf8d411846ad0dd8ee6
Mike Morrone <Hanegwen725@gmail.com>
2018-04-15 08:33:41 -0500

    UI Manager V1 almost done

---
+++ Assets/Scripts/Managers/RoundManager.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoundManager : MonoBehaviour
+{
+    public enum RoundState {VRPlayer, NonVRPlayer };
+    RoundState currentRoundState;
+    public RoundState CurrentRoundState
+    {
+        get
+        {
+            return currentRoundState;
+        }
+    }hgghj
+
+    int maxRounds;
+    int currentRound;
+    public int CurrentRound
+    {
+        get
+        {
+            return currentRound;
+        }
+    }
+
+    [SerializeField]
+    int defaultRoundTimer;
+
+    int roundTimer;
+    public int RoundTimer
+    {
+        get
+        {
+            return roundTimer;
+        }
+    }
+
+    int player1Score;
+    public int Player1Score
+    {
+        get
+        {
+            return player1Score;
+        }
+    }
+
+    int player2Score;
+    public int Player2Score
+    {
+        get
+        {
+            return player2Score;
+        }
+    }
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(roundTimer <= 0)
+        {
+            NextRound();
+        }
+	}
+
+    void NextRound()
+    {
+        
+        roundTimer = defaultRoundTimer;
+        StartCoroutine(Timer());
+
+        if (currentRoundState == RoundState.NonVRPlayer)
+        {
+            currentRound++;
+            currentRoundState = RoundState.VRPlayer;
+        }
+        else
+        {
+            currentRoundState = RoundState.NonVRPlayer;
+        }
+    }
+
+    IEnumerator Timer()
+    {
+        do
+        {
+            yield return new WaitForSeconds(1);
+            roundTimer--;
+        }
+        while (roundTimer >= 1);
+    }
+}
--- Assets/Scripts/Managers/UIManager.cs
+++ Assets/Scripts/Managers/UIManager.cs
@@ -1,19 +1,48 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
+    [SerializeField]
+    Text timerText; //Done
+    [SerializeField] //says VR or Non VR
+    Text RoundText; //Done
+    [SerializeField]
+    Text RoundNumberText; //Done
+    [SerializeField]
+    Text Player1Points;
+    [SerializeField]
+    Text Player2Points;
+
+    [SerializeField]
+    Text VRPowersLeft;
+
+    [SerializeField]
+    Text NonVRPowersLeft;
+
+    RoundManager rm;
 
 	// Use this for initialization
 	void Start ()
     {
+        rm = FindObjectOfType<RoundManager>();
 		working on UI Manager
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-		
+        UpdateUI();
 	}
+
+    void UpdateUI()
+    {
+        timerText.text = rm.RoundTimer.ToString();
+        RoundNumberText.text = "Round #" +  rm.CurrentRound.ToString();
+        RoundText.text = rm.CurrentRoundState.ToString();
+        Player1Points.text = "Player 1: " + rm.Player1Score.ToString();
+        Player2Points.text = "Player 2: " + rm.Player2Score.ToString();
+    }
 }
